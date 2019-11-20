61045dff5b2cc1637ef15e84c0d95e54852a73aa
Jeung Woo <odav12@naver.com>
2019-11-20 16:29:37 +0900

    내용물추가

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float startPos;
+    [SerializeField] float endPos;
+
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x <= endPos)
+        {
+            transform.Translate(-1 * (endPos - startPos), 0, 0);
+
+            SendMessage("ChangeHeight", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
